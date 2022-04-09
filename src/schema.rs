6e50799f4b974dfe3cb268d8afb8d9e261e8eecc
Oliver Daff <oliverdaff@users.noreply.github.com>
2022-04-09 15:47:26 +1000

    added primative types

--- src/schema.rs
+++ src/schema.rs
@@ -1,13 +1,17 @@
 use lazy_static::lazy_static;
 use regex::Regex;
-use serde::{de, Deserialize, Deserializer, Serialize};
+use serde::{
+    de::{self, IntoDeserializer},
+    Deserialize, Deserializer, Serialize,
+};
 
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(untagged, rename_all = "lowercase")]
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+#[serde(remote = "PrimativeType")]
 /// Primiative Types within a schemam.
 enum PrimativeType {
     /// True or False
-    Bool,
+    Boolean,
     /// 32-bit signed integer
     Int,
     /// 64-bit signed integer
@@ -18,7 +22,7 @@ enum PrimativeType {
     Double,
     /// Fixed point decimal
     /// TODO: Create this in spark and see what it looks like in the schema
-    Decimal(DecimalType),
+    Decimal { precision: i32, scale: u8 },
     /// Calendar date without timezone or time.
     Date,
     /// Time of day without date or timezone.
@@ -32,76 +36,96 @@ enum PrimativeType {
     /// Universally Unique Identifiers
     Uuid,
     /// Fixed length byte array
-    Fixed(FixedType),
+    Fixed(u64),
     /// Arbitray-lenght byte array.
     Binary,
 }
 
-#[derive(Debug, Serialize)]
-struct DecimalType {
-    precision: i32,
-    scale: u8,
+impl Serialize for PrimativeType {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        use PrimativeType::*;
+        match self {
+           Decimal {
+                precision: p,
+                scale: s,
+            } => serializer.serialize_str(&format!("decimal({p},{s})")),
+            Fixed(l) => serializer.serialize_str(&format!("fixed[{l}]")),
+            _ => PrimativeType::serialize(&self, serializer)
+}
+    }
 }
 
-impl<'de> Deserialize<'de> for DecimalType {
+impl<'de> Deserialize<'de> for PrimativeType {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: Deserializer<'de>,
     {
-        let this = String::deserialize(deserializer)?;
-        lazy_static! {
-            static ref RE: Regex = Regex::new(r#"^decimal\((?P<p>\d+),(?P<s>\d+)\)$"#).unwrap();
+        let s = String::deserialize(deserializer)?;
+        if s.starts_with("decimal") {
+            deserialize_decimal(s.into_deserializer())
+        } else if s.starts_with("fixed") {
+            deserialize_fixed(s.into_deserializer())
+        } else {
+            PrimativeType::deserialize(s.into_deserializer())
         }
-
-        let err_msg = format!("Invalid decimal format {}", this);
-
-        let caps = RE.captures(&this).ok_or(de::Error::custom(&err_msg))?;
-        let precision: i32 = caps
-            .name("p")
-            .ok_or(de::Error::custom(&err_msg))
-            .and_then(|p| {
-                p.as_str()
-                    .parse()
-                    .map_err(|_| de::Error::custom("precision not i32"))
-            })?;
-        let scale: u8 = caps
-            .name("s")
-            .ok_or(de::Error::custom(&err_msg))
-            .and_then(|p| {
-                p.as_str()
-                    .parse()
-                    .map_err(|_| de::Error::custom("scale not u8"))
-            })?;
-        Ok(DecimalType { precision, scale })
     }
 }
 
-#[derive(Debug, Serialize)]
-struct FixedType(u64);
-
-impl<'de> Deserialize<'de> for FixedType {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let this = String::deserialize(deserializer)?;
-        lazy_static! {
-            static ref RE: Regex = Regex::new(r#"^fixed\[(?P<l>\d+)\]$"#).unwrap();
-        }
-
-        let err_msg = format!("Invalid fixed format {}", this);
-
-        let caps = RE.captures(&this).ok_or(de::Error::custom(&err_msg))?;
-        let length: u64 = caps
-            .name("l")
-            .ok_or(de::Error::custom(&err_msg))
-            .and_then(|p| {
-                p.as_str()
-                    .parse()
-                    .map_err(|_| de::Error::custom("length not u64"))
-            })?;
-        Ok(FixedType(length))
+fn deserialize_decimal<'de, D>(deserializer: D) -> Result<PrimativeType, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let this = String::deserialize(deserializer)?;
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r#"^decimal\((?P<p>\d+),(?P<s>\d+)\)$"#).unwrap();
     }
+
+    let err_msg = format!("Invalid decimal format {}", this);
+
+    let caps = RE.captures(&this).ok_or(de::Error::custom(&err_msg))?;
+    let precision: i32 = caps
+        .name("p")
+        .ok_or(de::Error::custom(&err_msg))
+        .and_then(|p| {
+            p.as_str()
+                .parse()
+                .map_err(|_| de::Error::custom("precision not i32"))
+        })?;
+    let scale: u8 = caps
+        .name("s")
+        .ok_or(de::Error::custom(&err_msg))
+        .and_then(|p| {
+            p.as_str()
+                .parse()
+                .map_err(|_| de::Error::custom("scale not u8"))
+        })?;
+    Ok(PrimativeType::Decimal { precision, scale })
+}
+
+fn deserialize_fixed<'de, D>(deserializer: D) -> Result<PrimativeType, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let this = String::deserialize(deserializer)?;
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r#"^fixed\[(?P<l>\d+)\]$"#).unwrap();
+    }
+
+    let err_msg = format!("Invalid fixed format {}", this);
+
+    let caps = RE.captures(&this).ok_or(de::Error::custom(&err_msg))?;
+    let length: u64 = caps
+        .name("l")
+        .ok_or(de::Error::custom(&err_msg))
+        .and_then(|p| {
+            p.as_str()
+                .parse()
+                .map_err(|_| de::Error::custom("length not u64"))
+        })?;
+    Ok(PrimativeType::Fixed(length))
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -181,10 +205,10 @@ mod tests {
         let result_struct = serde_json::from_str::<StructField>(data).unwrap();
         assert!(matches!(
             result_struct.field_type,
-            PrimativeType::Decimal(DecimalType {
+            PrimativeType::Decimal {
                 precision: 1,
                 scale: 1
-            })
+            }
         ));
 
         let invalid_decimal_data = r#"
@@ -199,6 +223,20 @@ mod tests {
     }
 
     #[test]
+    fn test_boolean() {
+        let data = r#"
+        {
+            "id" : 1,
+            "name": "struct_name",
+            "required": true,
+            "field_type": "boolean"
+        }
+        "#;
+        let result_struct = serde_json::from_str::<StructField>(data).unwrap();
+        assert!(matches!(result_struct.field_type, PrimativeType::Boolean));
+    }
+
+    #[test]
     fn test_fixed() {
         let data = r#"
         {
@@ -209,10 +247,7 @@ mod tests {
         }
         "#;
         let result_struct = serde_json::from_str::<StructField>(data).unwrap();
-        assert!(matches!(
-            result_struct.field_type,
-            PrimativeType::Fixed(FixedType(1))
-        ));
+        assert!(matches!(result_struct.field_type, PrimativeType::Fixed(1),));
 
         let invalid_fixed_data = r#"
         {
@@ -223,5 +258,39 @@ mod tests {
         }
         "#;
         assert!(serde_json::from_str::<StructField>(invalid_fixed_data).is_err());
+    }
+
+    #[test]
+    fn test_all_valid_types() {
+        let type_mappings = vec![
+            PrimativeType::Boolean,
+            PrimativeType::Int,
+            PrimativeType::Long,
+            PrimativeType::Float,
+            PrimativeType::Double,
+            PrimativeType::Decimal{precision: 1, scale: 2},
+            PrimativeType::Date,
+            PrimativeType::Time,
+            PrimativeType::Timestamp,
+            PrimativeType::Timestampz,
+            PrimativeType::String,
+            PrimativeType::Uuid,
+            PrimativeType::Fixed(1),
+            PrimativeType::Binary,
+        ];
+
+        for primative in type_mappings {
+            let sf = StructField {
+                id: 1,
+                name: "name".to_string(),
+                required: true,
+                field_type: primative.clone(),
+                doc: None,
+            };
+
+            let j = serde_json::to_string(&sf).unwrap();
+            let unserde: StructField = serde_json::from_str(&j).unwrap();
+            assert_eq!(unserde.field_type, primative);
+        }
     }
 }
