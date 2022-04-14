3316f1a112689b30ad4459ccf0acab74fa43d7cf
Oliver Daff <oliverdaff@users.noreply.github.com>
2022-04-15 06:56:28 +1000

    added basic package docs (#22)

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 #![deny(missing_docs)]
 /*!
-A Rust implementation of Apache Iceberg.
+# Iceberg-rs
+
+Iceberg-rs is a library for working with [Apache Iceberg](https://iceberg.apache.org/).
+
+The Iceberg-rs consists of data structures that know how to
+serialise and deserialise the Iceberg table format.
 
 Currently V2 Table Metadata has been implemented.
 * !*/
