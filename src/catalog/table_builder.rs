bb0a21f5387e825a53d3e7353032e8cf3fd2d0ea
Jan <jankaul@mailbox.org>
2022-09-01 20:09:18 +0200

    new method for TableBuilder

--- src/catalog/table_builder.rs
+++ src/catalog/table_builder.rs
@@ -2,7 +2,14 @@
 Defining the [TableBuilder] trait for creating catalog tables and starting create/replace transactions
 */
 
-use crate::model::{partition::PartitionSpec, table::TableMetadataV2};
+use std::time::SystemTime;
+
+use uuid::Uuid;
+
+use crate::error::{IcebergError, Result};
+use crate::model::partition::{PartitionField, Transform};
+use crate::model::sort::{NullOrder, SortDirection, SortField, SortOrder};
+use crate::model::{partition::PartitionSpec, schema::SchemaV2, table::TableMetadataV2};
 
 ///Builder pattern to create a table
 pub struct TableBuilder {
@@ -10,6 +17,51 @@ pub struct TableBuilder {
 }
 
 impl TableBuilder {
+    /// Creates a new [TableBuilder] with some default metadata entries already set.
+    pub fn new(location: String, schema: SchemaV2) -> Result<Self> {
+        let partition_spec = PartitionSpec {
+            spec_id: 1,
+            fields: vec![PartitionField {
+                name: "default".to_string(),
+                field_id: 1,
+                source_id: 1,
+                transform: Transform::Void,
+            }],
+        };
+        let sort_order = SortOrder {
+            order_id: 1,
+            fields: vec![SortField {
+                source_id: 1,
+                transform: Transform::Void,
+                direction: SortDirection::Descending,
+                null_order: NullOrder::Last,
+            }],
+        };
+        let metadata = TableMetadataV2 {
+            table_uuid: Uuid::new_v4(),
+            location: location,
+            last_sequence_number: 0,
+            last_updated_ms: SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .map_err(|err| IcebergError::Message(err.to_string()))?
+                .as_millis() as i64,
+            last_column_id: schema.struct_fields.fields.len() as i32,
+            schemas: vec![schema],
+            current_schema_id: 1,
+            partition_specs: vec![partition_spec],
+            default_spec_id: 1,
+            last_partition_id: 1,
+            properties: None,
+            current_snapshot_id: None,
+            snapshots: None,
+            snapshot_log: None,
+            metadata_log: None,
+            sort_orders: vec![sort_order],
+            default_sort_order_id: 0,
+            refs: None,
+        };
+        Ok(TableBuilder { metadata: metadata })
+    }
     /// Sets a partition spec for the table.
     pub fn with_partition_spec(mut self, partition_spec: PartitionSpec) -> Self {
         self.metadata.partition_specs.push(partition_spec);
