f771e2f03c984604ba83da3a4aa7496b32343034
Jan <jankaul@mailbox.org>
2022-09-06 19:58:13 +0200

    struct list  and map type

--- src/model/types/struct_field.rs
+++
@@ -1,8 +0,0 @@
-/*!
- * Struct type in iceberg
- */
-
-use super::Nullable;
-
-/// Struct field is tuple of name, value, and optional documentation.
-pub type StructField = (String, Nullable, Option<String>);
