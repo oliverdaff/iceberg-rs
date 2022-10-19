a9f2acc64db15c452b981496e9c2dde1433f7ca4
JanKaul <jankaul@mailbox.org>
2022-10-19 19:10:28 +0200

    execute for view operations

--- src/view/transaction/operation.rs
+++ src/view/transaction/operation.rs
@@ -4,7 +4,10 @@
 
 use anyhow::Result;
 
-use crate::{model::schema::Schema, view::View};
+use crate::{
+    model::{schema::Schema, view_metadata::ViewMetadata},
+    view::View,
+};
 
 /// View operation
 pub enum Operation {
@@ -12,15 +15,38 @@ pub enum Operation {
     UpdateSchema(Schema),
     // /// Update table properties
     // UpdateProperties,
-    // /// Update the table location
-    // UpdateLocation,
+    /// Update the table location
+    UpdateLocation(String),
 }
 
 impl Operation {
     /// Execute operation
     pub async fn execute(self, view: &mut View) -> Result<()> {
         match self {
-            _ => Ok(()),
+            Operation::UpdateLocation(location) => {
+                view.metadata_location = location;
+                Ok(())
+            }
+            Operation::UpdateSchema(schema) => match &mut view.metadata {
+                ViewMetadata::V1(metadata) => match &mut metadata.schemas {
+                    None => {
+                        metadata.current_schema_id = match &schema {
+                            Schema::V1(schema) => schema.schema_id,
+                            Schema::V2(schema) => Some(schema.schema_id),
+                        };
+                        metadata.schemas = Some(vec![schema]);
+                        Ok(())
+                    }
+                    Some(schemas) => {
+                        metadata.current_schema_id = match &schema {
+                            Schema::V1(schema) => schema.schema_id,
+                            Schema::V2(schema) => Some(schema.schema_id),
+                        };
+                        schemas.push(schema);
+                        Ok(())
+                    }
+                },
+            },
         }
     }
 }
