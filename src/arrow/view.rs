18fa8ddfa0b6be8d036d9f34809fd4ebad38462c
JanKaul <jankaul@mailbox.org>
2022-10-20 15:03:36 +0200

    test view

--- src/arrow/view.rs
+++ src/arrow/view.rs
@@ -45,3 +45,97 @@ impl DataFusionView {
         .map_err(anyhow::Error::msg)
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use std::sync::Arc;
+
+    use datafusion::{
+        arrow::{array::Float32Array, record_batch::RecordBatch},
+        prelude::SessionContext,
+    };
+    use object_store::{local::LocalFileSystem, memory::InMemory, ObjectStore};
+
+    use crate::{
+        datafusion::DataFusionTable,
+        model::schema::{AllType, PrimitiveType, SchemaStruct, SchemaV2, StructField},
+        table::Table,
+        view::view_builder::ViewBuilder,
+    };
+
+    use super::*;
+
+    #[tokio::test]
+    pub async fn test_datafusion_view_scan() {
+        let local_object_store: Arc<dyn ObjectStore> =
+            Arc::new(LocalFileSystem::new_with_prefix("./tests").unwrap());
+        let memory_object_store: Arc<dyn ObjectStore> = Arc::new(InMemory::new());
+
+        let table = Arc::new(DataFusionTable::from(
+            Table::load_file_system_table("/home/iceberg/warehouse/nyc/taxis", &local_object_store)
+                .await
+                .unwrap(),
+        ));
+
+        let ctx = SessionContext::new();
+
+        ctx.register_table("nyc_taxis", table).unwrap();
+
+        let schema = SchemaV2 {
+            schema_id: 1,
+            identifier_field_ids: Some(vec![1]),
+            name_mapping: None,
+            struct_fields: SchemaStruct {
+                fields: vec![StructField {
+                    id: 1,
+                    name: "trip_distance".to_string(),
+                    required: false,
+                    field_type: AllType::Primitive(PrimitiveType::Float),
+                    doc: None,
+                }],
+            },
+        };
+
+        let view = Arc::new(
+            DataFusionView::from(
+                ViewBuilder::new_filesystem_view(
+                    "SELECT trip_distance FROM nyc_taxis",
+                    "test/view1",
+                    schema,
+                    Arc::clone(&memory_object_store),
+                )
+                .unwrap()
+                .commit()
+                .await
+                .unwrap(),
+            )
+            .to_view_table(&ctx)
+            .unwrap(),
+        );
+
+        ctx.register_table("nyc_taxis_trip_distance", view).unwrap();
+
+        let df = ctx
+            .sql("SELECT MIN(trip_distance) FROM nyc_taxis_trip_distance")
+            .await
+            .unwrap();
+
+        // execute the plan
+        let results: Vec<RecordBatch> = df.collect().await.expect("Failed to execute query plan.");
+
+        let batch = results
+            .into_iter()
+            .find(|batch| batch.num_rows() > 0)
+            .expect("All record batches are empty");
+
+        let values = batch
+            .column(0)
+            .as_any()
+            .downcast_ref::<Float32Array>()
+            .expect("Failed to get values from batch.");
+
+        // Value can either be 0.9 or 1.8
+        assert!(((1.35 - values.value(0)).abs() - 0.45).abs() < 0.001)
+    }
+}
